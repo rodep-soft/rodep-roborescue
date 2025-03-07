a095f09197936f7553c9af95b25e10d8e74c6cb1
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2025-03-07 10:55:54 +0000

    test

---
+++ yano_ws/build/custom_interfaces/colcon_command_prefix_test.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/root/working/rodep_ros2/yano_ws/install/custom_interfaces/share/custom_interfaces/package.sh"
---
+++ yano_ws/build/driver/colcon_command_prefix_test.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/root/working/rodep_ros2/yano_ws/install/custom_interfaces/share/custom_interfaces/package.sh"
+. "/root/working/rodep_ros2/yano_ws/install/driver/share/driver/package.sh"

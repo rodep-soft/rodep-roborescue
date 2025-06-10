c968920f2b017855a2049752282f7bfb704f3554
Tatsuki Yano <yano.tatsuki439@mail.kyutech.jp>
2025-06-10 13:03:14 +0900

    Delete main_ws/build directory
    remove build/

--- main_ws/build/flipper_driver/CMakeFiles/flipper_driver_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/flipper_driver_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/flipper_driver_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- main_ws/build/my_rqt_plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rodep/rodep/test_ws/yano_ws/src/my_rqt_plugin
-# Build directory: /home/rodep/rodep/test_ws/yano_ws/build/my_rqt_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
